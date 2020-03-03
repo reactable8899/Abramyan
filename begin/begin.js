7b84b3702ef1379b7883d98d1a3b6cc8351adfd8
reactable8899 <olim.haydarov8899@gmail.com>
2020-03-04 01:54:29 +0500

    begin15

---
+++ begin/begin.js
@@ -0,0 +1,74 @@
+//begin#1
+// var a = process.argv[2];
+// console.log(4*a)
+//#2
+// var a = process.argv[2];
+// console.log(a*a)
+//#3
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var p = 2*a+2*b;
+// var s = a*b;
+// console.log("P = " + p + " S = " + s)
+//#4
+// var p = 3.14;
+// var d = process.argv[2];
+// console.log(p*d)
+//#5
+// var a = process.argv[2];
+// var v = a*a*a;
+// var s = 6*a*a;
+// console.log(v + " " + s)
+//#6
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var c = process.argv[4];
+// var v = a*b*c;
+// var s = 2*(a*b+b*c+c*d);
+// console.log(s)
+//#7
+// var p = 3.14;
+// var r = process.argv[2];
+// console.log(2*p*r)
+//8
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var s = (+a + +b) / 2;
+// console.log(s);
+//9
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var s = Math.sqrt(a*b);
+// console.log(s)
+//#10
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var sum = +a + +b;
+// var razn = a - b;
+// var um = a*b;
+// console.log(sum + " " + razn + " "+ um)
+//#11
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var c = Math.sqrt(a*a+b*b)
+// var p = +a + +b + +c;
+// console.log(c + " " + p)
+//#12
+// var a = process.argv[2];
+// var b = process.argv[3];
+// var p = 3.14;
+// var s1 = p*a*a;
+// var s2 = p*b*b;
+// console.log("s3 =" + (s1 - s2))
+//#13
+// var p = 3.14;
+// var r = process.argv[2];
+// var l = 2*p*r;
+// var s = 2*p*r*r;
+// console.log(l + " " + s)
+//#14
+// var p = 3.14;
+// var r = process.argv[2];
+// var l = 4*p*r;
+// var s = 4*p*r*r;
+// console.log(l + " " + s)
