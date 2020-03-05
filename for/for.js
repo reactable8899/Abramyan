8849d466d18d0a38584a53fad9b3f0d9c952c840
reactable8899 <olim.haydarov8899@gmail.com>
2020-03-05 21:19:42 +0500

    update

--- for/for.js
+++ for/for.js
@@ -92,11 +92,114 @@
 //   s+=1/i;
 // }
 // console.log(s)
-//#11
-var n = process.argv[2];
-n = parseInt(n);
-var s = 0;
-for(var i = 1;i<=n;i++){
-  s+=(n+i)(n+i);
-}
-console.log(s)
+//#12
+// var n = process.argv[2];
+// n = parseInt(n);
+// var s = 1;
+// for(var i = 1;i<=n;i++){
+//   s*=((10+i)/10)
+// }
+// console.log(s)
+//#13
+// var n = process.argv[2];
+// n = parseInt(n);
+// var s = 0;
+// for(var i = 1;i<=n;i+2){
+//   s+=(((10+i)/10)-((10+i+1)/10));
+// }
+// console.log(s)
+//#14
+// var n = process.argv[2];
+// n = parseInt(n);
+// var s = 0;
+// for(var i = 1;i<=n;i++){
+//   s+=2*i-1;
+// }
+// console.log(s)
+//#15
+// var a = process.argv[2];
+// a = parseInt(a);
+// var n = process.argv[3];
+// n = parseInt(n);
+// var s = 1;
+// for(var i = 1;i<=n;i++){
+// s*=a;
+// }
+// console.log(s)
+//#16
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var n = process.argv[3];
+// n = parseFloat(n);
+// var s = 1;
+// for(var i = 1;i<=n;i++){
+// s*=a;
+// console.log(s)
+// }
+//#17
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var n = process.argv[3];
+// n = parseFloat(n);
+// var s = 1;
+// var x = 1;
+// for(var i = 1;i<=n;i++){
+// x*=a;
+// s+=x;
+// console.log(s)
+// }
+//#18
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var n = process.argv[3];
+// n = parseFloat(n);
+// var s = 2;
+// var x = 2;
+// var t = 2;
+// for(var i = 2;i<n;i++){
+//   x*=a;
+//   t*=x;
+// console.log(x+" "+ t + " ")
+// }
+//#19
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var s = 1;
+// for(var i = 1;i<=a;i++){
+//   s*=i;
+//   console.log(s)
+// }
+//#20
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var s = 1;
+// var n = 0;
+// for(var i = 1;i<=a;i++){
+//   s*=i;
+//   n+=s;
+//   console.log(n)
+// }
+//#21
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var s = 1;
+// var n = 1;
+// for(var i = 1;i<=a;i++){
+//   s*=i;
+//   n+=1/s;
+//   console.log(n)
+// }
+//#22
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var b = process.argv[3];
+// b = parseFloat(b);
+// var s = 1;
+// var x = 1;
+// var t = 1;
+// for(var i = 1;i<=b;i++){
+// x*=a;
+// t*=i;
+// s+=x/t;
+//   console.log(s)
+// }
