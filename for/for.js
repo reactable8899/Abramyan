68cd009050b449b0265e3f1b7851addc9626ab93
reactable8899 <olim.haydarov8899@gmail.com>
2020-03-05 18:25:06 +0500

    3

--- for/for.js
+++ for/for.js
@@ -39,13 +39,64 @@
 //   console.log(s);
 // }
 //#5
-var k = process.argv[3];
-var s=0;
-k = parseInt(k);
+// var k = process.argv[2];
+// var s=0;
+// k = parseInt(k);
+// for(var i = 1;i<=10;i++){
+//   s = k * i /10
+//   console.log(s);
+// }
+//#6
+// var k = process.argv[2];
+// var s=0;
+// k = parseInt(k);
+// for(var i = 6;i<=10;i++){
+//   s = k * i / 10
+//   console.log(s);
+// }
+//#7
+// var n = process.argv[2];
+// var k = process.argv[3];
+// var s=0;
+// k = parseInt(k);
+// n = parseInt(n);
+// for(var i = n;i<=k;i++){
+//   s+=i;
+// }
+// console.log(s);
+//#8
+// var n = process.argv[2];
+// var k = process.argv[3];
+// var s=1;
+// k = parseInt(k);
+// n = parseInt(n);
+// for(var i = n;i<=k;i++){
+//   s*=i;
+// }
+// console.log(s);
+//#9
+// var n = process.argv[2];
+// var k = process.argv[3];
+// var s=0;
+// k = parseInt(k);
+// n = parseInt(n);
+// for(var i = n;i<=k;i++){
+//   s+=i*i;
+// }
+// console.log(s);
+//#10
+// var n = process.argv[2];
+// n = parseInt(n);
+// var s = 0;
+// for(var i = 1;i<=n;i++){
+//   s+=1/i;
+// }
+// console.log(s)
+//#11
+var n = process.argv[2];
 n = parseInt(n);
-for(var i = 1;i<10;i++){
-  s = k
-  console.log(i);
-  s++;
+var s = 0;
+for(var i = 1;i<=n;i++){
+  s+=(n+i)(n+i);
 }
-console.log("kol " +s);
+console.log(s)
