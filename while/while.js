479642d65076dbbf4f19dc678c9631c406807b76
reactable8899 <olim.haydarov8899@gmail.com>
2020-03-09 19:52:17 +0500

    while1

---
+++ while/while.js
@@ -0,0 +1,103 @@
+//while
+//#1
+// var a = process.argv[2];
+// a = parseInt(a);
+// var b = process.argv[3];
+// b = parseInt(b);
+// var temp = a;
+// while(temp>=0){
+//   temp-=b;
+// }
+// console.log(temp+b)
+//#2
+// var a = process.argv[2];
+// a = parseInt(a);
+// var b = process.argv[3];
+// b = parseInt(b);
+// var temp = a;
+// var i = 0;
+// while(temp>=0){
+//   temp-=b;
+//   i++;
+// }
+// console.log(i-1)
+//#3
+// var a = process.argv[2];
+// a = parseFloat(a);
+// var b = process.argv[3];
+// b = parseFloat(b);
+// var temp = a;
+// var i = 0;
+// var s = 0;
+// while(temp>=0){
+//   temp-=b;
+//   i++;
+//   s+=b;
+// }
+// console.log(i-1)
+// console.log(a-s+b)
+//#4
+// var a = process.argv[2];
+// a = parseInt(a);
+// var temp = a;
+// var s = 1;
+// var n = 1;
+// while(a>3){
+//   if((a%3)==0){
+//   a/=3;
+//   s++;
+// }
+// else {
+//   console.log("false")
+//   return 0;
+// }
+// }
+// for(var i = 1; i<=s;i++)
+// n*=3;
+// if(n==temp){
+// console.log("true")
+// }
+// else {
+//   console.log("false")
+// }
+//#5
+// var a = process.argv[2];
+// a = parseInt(a);
+// var temp = 0;
+// var s = 1;
+// while(a>2){
+//   temp=a/2;
+//   s++;
+//   a-=temp;
+//   //console.log(s)
+// }
+// console.log(s)
+//#7
+// var a = process.argv[2];
+// a = parseInt(a);
+// var s = 1;
+// var n = 1;
+// while(a>s){
+//     s = n*n;
+//     n++;
+//   }
+//   console.log(n-1)
+//#8
+// var a = process.argv[2];
+// a = parseInt(a);
+// var s = 1;
+// var n = 1;
+// while(a>s){
+//     s = n*n;
+//     n++;
+//   }
+//   console.log(n-2)
+//#9
+var a = process.argv[2];
+a = parseInt(a);
+var s = 1;
+var n = 1;
+while(a>s){
+    
+
+}
