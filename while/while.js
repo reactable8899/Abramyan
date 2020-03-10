cab6796a30d2fe4f1ed0563b1ab730f4f7e5953c
reactable8899 <olim.haydarov8899@gmail.com>
2020-03-10 12:18:39 +0500

    Scode

--- while/while.js
+++ while/while.js
@@ -1,28 +1,28 @@
-//while
-//#1
+// while
+// #1
 // var a = process.argv[2];
 // a = parseInt(a);
 // var b = process.argv[3];
 // b = parseInt(b);
 // var temp = a;
-// while( temp >= 0 ){
+// while ( temp >= 0 ) {
 //   temp -= b;
 // }
-// console.log(temp+b)
-
-//#2
-var a = process.argv[2];
-a = parseInt(a);
-var b = process.argv[3];
-b = parseInt(b);
-var i = 0;
-while( a >= 0 ){
-  a -= b;
-  i++;
-}
-console.log(i-1)
-
-//#3
+// console.log( temp + b )
+//
+// #2
+// var a = process.argv[2];
+// a = parseInt(a);
+// var b = process.argv[3];
+// b = parseInt(b);
+// var i = 0;
+// while ( a >= 0 ) {
+//   a -= b;
+//   i++;
+// }
+// console.log( i - 1 )
+//
+// #3
 // var a = process.argv[2];
 // a = parseFloat(a);
 // var b = process.argv[3];
@@ -30,163 +30,161 @@ console.log(i-1)
 // var temp = a;
 // var i = 0;
 // var s = 0;
-// while( temp >= 0 ){
+// while ( temp >= 0 ) {
 //   temp -= b;
 //   i++;
 //   s += b;
 // }
-// console.log(i-1)
-// console.log(a-s+b)
-
-//#4
+// console.log( i - 1 )
+// console.log( a - s + b )
+//
+// #4
 // var a = process.argv[2];
 // a = parseInt(a);
 // var temp = a;
 // var s = 1;
 // var n = 1;
-// while( a > 3 ){
-//   if(( a % 3 ) == 0){
+// while ( a > 3 ) {
+//   if ( ( a % 3 ) == 0) {
 //   a /= 3;
 //   s++;
-//   }
-//   else {
-//   console.log("false")
+//   } else {
+//   console.log( "false" )
 //   return 0;
 //   }
 // }
-// for( var i = 1; i <= s; i++ )
+// for ( var i = 1; i <= s; i++ )
 //   n *= 3;
-//   if( n == temp ){
+//   if ( n == temp ) {
 //   console.log("true")
-//   }
-// else {
+//   } else {
 //   console.log("false")
 // }
-
-//#5
+//
+// #5
 // var a = process.argv[2];
 // a = parseInt(a);
 // var temp = 0;
 // var s = 1;
-// while( a > 2 ){
+// while ( a > 2 ) {
 //   temp = a / 2;
 //   s++;
 //   a -= temp;
 //   //console.log(s)
 // }
 // console.log(s)
-
-//#7
+//
+// #7
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 1;
-// while( a > s * s ){
+// while ( a > s * s ) {
 //   s++;
 // }
 // console.log(s)
-
-//#8
+//
+// #8
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 1;
-// while( a >= s * s ){
+// while ( a >= s * s ) {
 //   s++;
 //   console.log(s)
 // }
-
-//#9
+//
+// #9
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 1;
 // var k = 0;
-// while( a > s ){
+// while ( a > s ) {
 //   s *= 3;
 //   k++;
 // }
 // console.log(k)
-
-//#10
+//
+// #10
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 1;
 // var k = 0;
-// while( a > s ){
+// while ( a > s ) {
 //   s *= 3;
 //   k++;
 // }
 // console.log(k-1)
-
-//#11
+//
+// #11
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 0;
 // var n = 1;
-// while( a > s ){
+// while ( a > s ) {
 //   s += n;
 //   n++;
 //   console.log(s + " " + (n-1))
 // }
-
-//#12
+//
+// #12
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 0;
 // var n = 1;
-// while( s <= a ){
+// while ( s <= a ) {
 //   console.log(s)
 //   s += n;
 //   n++;
 // }
-
-//#13
+//
+// #13
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 0;
 // var n = 1;
-// while( s < a ){
+// while ( s < a ) {
 //   s += 1 / n;
 //   n++;
 //   console.log(s)
 // }
-
-//#14
+//
+// #14
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s = 0;
 // var n = 1;
-// while( s <= a ){
+// while ( s <= a ) {
 //   console.log(s + " " + n)
 //   s += 1 / n;
 //   n++;
 // }
-
-//#15
+//
+// #15
 // var a = process.argv[2];
 // a = parseInt(a);
 // var n = 1000;
 // var month = 0;
-// while( n < 1100 ){
+// while ( n < 1100 ) {
 //   n += n * a / 100;
 //   month++;
 //   console.log(n + " " + month)
 // }
-
-//#16
+//
+// #16
 // var a = process.argv[2];
 // a = parseInt(a);
 // var n = 10;
 // var days = 1;
-// while( n < 200 ){
+// while ( n < 200 ) {
 //   console.log(n+ " "+ days)
 //   n += 10 + n * a / 100;
 //   days++;
 // }
-
-//#17
+//
+// #17
 // var a = process.argv[2];
 // a = parseInt(a);
 // var n = a;
-// while( a != a % 10 ){
+// while ( a != a % 10 ) {
 //   a = a % 10;
 //   b = n % 100;
 //   c = b / 10;
@@ -195,16 +193,15 @@ console.log(i-1)
 //   c = parseInt(c)
 //   console.log(a + " " + c + " "+d)
 // }
-
-//#22
+//
+// #22
 // var a = process.argv[2];
 // a = parseInt(a);
 // var s;
-// if( (a%2) ==1 ){
-//   if((a%3) == 2 || (a%3) == 1)
+// if ( ( a % 2 ) ==1 ) {
+//   if ( ( a % 3 ) == 2 || ( a%3 ) == 1)
 //   console.log("true")
-// }
-// else {
-//   if((a%3) == 2)
+// } else {
+//   if ( ( a % 3 ) == 2)
 //   console.log("true")
 // }
