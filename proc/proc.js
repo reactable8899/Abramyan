e9233c74eb580b3f83704a3768e4bd16863fd9f7
reactable8899 <olim.haydarov8899@gmail.com>
2020-03-15 22:21:38 +0500

    proc1

---
+++ proc/proc.js
@@ -0,0 +1,167 @@
+//PROC
+//#1
+// function proc1(a) {
+//   let b = a * a * a;
+//   console.log(b)
+// }
+// proc1(2);
+
+//#2
+// function proc2(a) {
+//   let b = a * a;
+//   let c = b * a;
+//   let d = c * a;
+//   console.log("b = " + b + " c = " + c + " d = " + d)
+// }
+// proc2(2);
+
+//#3
+// let a = 2;
+// let b = 5;
+// let c = 3;
+// let d = 4;
+// function proc3(a,b) {
+//   let aMean = (a + b) / 2;
+//   let gMean = (a * b);
+//   console.log(aMean);
+//   console.log(gMean);
+// }
+// proc3(a,b);
+// proc3(a,c);
+// proc3(a,d);
+
+//#4
+// function proc4(a) {
+//   let p = 3*a;
+//   let s = a*a*Math.sqrt(4)/4;
+//   console.log(p + " " + s)
+// }
+// proc4(2);
+
+//#6
+// var s = 0;
+// var t = 0;
+// var a = 12;
+// var k;
+// function proc5(c) {
+//   c = parseInt(c);
+//   while (c != 0) {
+//   s += c % 10;
+//   t++;
+//   c = c / 10;
+//   console.log(s + " " + t)
+//   }
+// }
+// proc5(1234)
+
+//#8
+// function proc8(k,d) {
+//   console.log(k)
+//   console.log(k+ "" +d)
+//   let d1 = 1;
+//   let d2 = 2;
+//   console.log(k+ "" +d +"" +d1)
+//   console.log(k+ "" +d + d1+ "" +d2)
+// }
+// proc8(2,5)
+
+//#9
+// function proc9(k,d) {
+//   console.log(k)
+//   console.log(d+ "" +k)
+//   let d1 = 1;
+//   let d2 = 2;
+//   console.log(d1+ "" +d +"" +k)
+//   console.log(d2+""+d1+ "" +d +"" +k)
+// }
+// proc8(2,5)
+
+//#10
+// function proc10(x,y) {
+//   let c;
+//   c = x;
+//   x = y;
+//   y = c;
+//   console.log(x+" "+y)
+// }
+// proc10(2,5)
+
+//#11
+// let a = 3;
+// let b = 4;
+// let c = 5;
+// let d = 6;
+// function proc11(x,y) {
+//   if (x > y) {
+//     min = y;
+//     max = x;
+//   } else {
+//     min = x;
+//     max = y;
+//   }
+//   console.log(min + " " + max)
+// }
+// proc11(5,3)
+// proc11(a,b)
+// proc11(b,c)
+// proc11(c,d)
+// proc11(a,d)
+
+//#14
+// function proc14(a,b,c) {
+//   let y = a,x = b,z = c;
+//   b = y;
+//   c = x;
+//   a = z;
+//   console.log(a+ " "+ b + " " +c)
+// }
+// proc14(2,3,4)
+
+//#15
+// function proc15(a,b,c) {
+//   let y = a,x = b,z = c;
+//   a = x;
+//   b = z;
+//   c = y;
+//   console.log(a+ " "+ b + " " +c)
+// }
+// proc15(2,3,4)
+
+//#16
+// function proc16(a) {
+//   let b;
+//   if (a > 0)
+//   console.log(1)
+//   if (a < 0)
+//   console.log(-1)
+//   if (a === 0)
+//   console.log(0)
+// }
+// proc16(1)
+
+//#17
+// function proc17(a,b,c) {
+//   let d = b*b-4*a*c;
+//   if (d > 0)
+//   console.log("Корней два")
+//   if (d === 0)
+//   console.log("1 корень")
+//   if (d < 0)
+//   console.log("2 комплексных корня")
+// }
+// proc17(1,3,1)
+
+//#18
+// function proc18(r) {
+//   let p = 3.14;
+//   let s = p*r*r;
+//   console.log(s)
+// }
+// proc18(2)
+
+//#20
+// function proc20(a,h) {
+//   let b = Math.sqrt((a/2)*(a/2) + h*h)
+//   console.log(b)
+// }
+// proc20(2,3)
